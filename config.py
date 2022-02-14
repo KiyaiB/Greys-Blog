be20268fd4397e104feae180fd368d09d7ca24b7
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-02-14 11:00:24 +0300

    modify config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaaccess:Kennedy@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
