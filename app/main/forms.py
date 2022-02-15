7499990b6affe7e1142b6e0644a45260e73d84e2
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-02-15 09:46:02 +0300

    Deleted an image

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField,RadioField,SelectField
-from wtforms.validators import Required
+from wtforms.validators import InputRequired
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Whats about yourself.',validators = [Required()])
+    bio = TextAreaField('Whats about yourself.',validators = [InputRequired()])
     submit = SubmitField('Submit')
     
 class PostForm(FlaskForm):
     post_title = StringField('Title')
-    description = TextAreaField('Description', validators=[Required()])
+    description = TextAreaField('Description', validators=[InputRequired()])
     submit = SubmitField('Submit')
     
 class CommentForm(FlaskForm):
-    comment = TextAreaField('write a comment',validators=[Required()])
+    comment = TextAreaField('write a comment',validators=[InputRequired()])
     submit = SubmitField('comment')
